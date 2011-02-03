26875a69b09cc5d59abcf09d1ba4c3769b86c3c1
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2011-02-03 09:40:51 +0000

    phpcs rules

---
+++ phpcs-standards/WebME/WebMECodingStandard.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * WebME Coding Standard.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   CVS: $Id: WebMECodingStandard.php 269131 2008-11-17 05:04:07Z squiz $
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+if (class_exists('PHP_CodeSniffer_Standards_CodingStandard', true) === false) {
+    throw new PHP_CodeSniffer_Exception('Class PHP_CodeSniffer_Standards_CodingStandard not found');
+}
+
+/**
+ * WebME Coding Standard.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   http://matrix.squiz.net/developer/tools/php_cs/licence BSD Licence
+ * @version   Release: 1.2.2
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class PHP_CodeSniffer_Standards_WebME_WebMECodingStandard extends PHP_CodeSniffer_Standards_CodingStandard
+{
+
+	public function getExcludedSniffs(){
+		return array(
+		);
+	}
+
+    /**
+     * Return a list of external sniffs to include with this standard.
+     *
+     * The WebME standard uses some generic sniffs.
+     *
+     * @return array
+     */
+    public function getIncludedSniffs()
+    {
+        return array(
+                'Generic/Sniffs/NamingConventions/UpperCaseConstantNameSniff.php',
+                'Generic/Sniffs/PHP/LowerCaseConstantSniff.php',
+                'Generic/Sniffs/PHP/DisallowShortOpenTagSniff.php'
+               );
+
+    }//end getIncludedSniffs()
+
+
+}//end class
+?>
