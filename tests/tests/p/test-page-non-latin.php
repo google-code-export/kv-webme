47228567c4419ed99809c18e2fc1d4fd6ee2635c
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2012-02-08 09:29:25 +0000

    add test for nested non-latin pages

--- tests/tests/p/test-page-non-latin.php
+++ tests/tests/p/test-page-non-latin.php
@@ -46,6 +46,37 @@ if ($file!=$expected) {
 	)));
 }
 // }
+// { add sub-page with non-latin name
+$file=Curl_get('http://kvwebmerun/a/f=adminPageEdit', array(
+	'parent'  =>6,
+	'name'    =>'áéíóúÁÉÍÓÚæøåSubpage',
+	'type'    =>0,
+	'body[en]'=>'<p>test non-latin page</p>'
+));
+$expected='{"id":"7","pid":6,"alias":"aeiouaeiouaoasubpage"}';
+if ($file!=$expected) {
+	die(json_encode(array(
+		'errors'=>'non-latin page not entered correctly.<br/>expected:<br/>'
+			.htmlspecialchars($expected).'<br/>actual:<br/>'.$file
+	)));
+}
+// }
+// { check new list of root pages
+$file=Curl_get('http://kvwebmerun/a/f=getMenu', array());
+$expected='[null,[{"subid":"1","id":"1","name":"test2","alias":"test2","type":"0","numchildren":"0","classes":"menuItemTop first","link":"\/test2","parent":null},{"subid":"4","id":"4","name":"redirect","alias":"redirect","type":"1","numchildren":"1","classes":"menuItemTop ajaxmenu_hasChildren","link":"\/redirect","parent":null},{"subid":"6","id":"6","name":"\u00e1\u00e9\u00ed\u00f3\u00fa\u00c1\u00c9\u00cd\u00d3\u00da\u00e6\u00f8\u00e5","alias":"aeiouaeiouaoa","type":"0","numchildren":"1","classes":"menuItemTop ajaxmenu_hasChildren","link":"\/aeiouaeiouaoa","parent":null}]]';
+if ($file!=$expected) {
+	die(json_encode(array(
+		'errors'=>'list of root pages doesn\'t show new sub-page.<br/>expected:<br/>'
+			.htmlspecialchars($expected).'<br/>actual:<br/>'.$file
+	)));
+}
+// }
+// { check that sub-page loads
+$file=Curl_get('http://kvwebmerun/aeiouaeiouaoa/aeiouaeiouaoasubpage', array());
+if (strpos($file, '<h1>áéíóúÁÉÍÓÚæøåSubpage</h1>')===false) {
+	die('{"errors":"failed to load new sub-page"}');
+}
+// }
 // { logout
 $file=Curl_get('http://kvwebmerun/a/f=logout', array());
 $file=Curl_get('http://kvwebmerun/ww.admin/', array());
