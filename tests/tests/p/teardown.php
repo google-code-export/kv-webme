485840fe7045688ac00e23f8c43d20759e3bd0fa
kae.verens <kae.verens@a0706164-33d4-2dc3-03d6-3f270faf84b9>
2012-03-29 15:47:08 +0000

    make the teardown a bit more through in its testing

--- tests/tests/p/teardown.php
+++ tests/tests/p/teardown.php
@@ -1,6 +1,5 @@
 <?php
 require_once '../config.php';
-echo '1';exit;
 $run_dir=realpath('../../run');
 function rrmdir($dir) {
 	$objects = scandir($dir);
@@ -14,6 +13,12 @@ function rrmdir($dir) {
 	rmdir($dir);
 }
 rrmdir($run_dir.'/trunk');
+if (file_exists($run_dir.'/trunk')) {
+	echo '{"errors":"could not remove trunk"}';
+}
 rrmdir($run_dir.'/files');
+if (file_exists($run_dir.'/files')) {
+	echo '{"errors":"could not remove files"}';
+}
 `echo "drop database kvwebmetest; create database kvwebmetest;" | mysql -uroot`;
 echo '{"ok":1,"notes":"actual time may vary"}';
